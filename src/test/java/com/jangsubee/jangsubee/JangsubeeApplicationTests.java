cec2e668cbbddec84b9b1fa8f91c9bdd2aa5e896
heeseok <nhs0912@nate.com>
2023-02-15 15:57:42 +0900

    feat : 스프링 설정파일 및 flyway 설정

---
+++ src/test/java/com/jangsubee/jangsubee/JangsubeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jangsubee.jangsubee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JangsubeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
