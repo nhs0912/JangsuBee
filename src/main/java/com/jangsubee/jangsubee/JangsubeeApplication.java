0b334eb2aca3e8373dfd20465ac83fa9a16dbc7b
HeeSeok Noh <heeseoknoh@HeeSeoks-MacBook-Pro15.local>
2023-02-15 14:59:21 +0900

    feat : init 초기 프로젝트

---
+++ src/main/java/com/jangsubee/jangsubee/JangsubeeApplication.java
@@ -0,0 +1,13 @@
+package com.jangsubee.jangsubee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JangsubeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JangsubeeApplication.class, args);
+	}
+
+}
